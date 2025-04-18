a0bb4d19e785dfe89a726d2a5893df59025b85e7
ranelin4r <fbwarkup@outlook.com>
2025-04-19 04:46:27 +0800

    fix

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ window.addEventListener('load', function() {
     this.setTimeout(() => {
         document.body.removeChild(load);
     },1000);
-<<<<<<< HEAD
 
     lenisInit();
 });
@@ -20,25 +19,10 @@ function lenisInit() {
 function showNav() {
     const mobileNavbar = document.getElementById('mobile-navbar');
     if (mobileNavbar.classList.contains('hidden')) {
-=======
-    document.getElementById('main').style.transform = 'translateY(0)';
-    document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightElement(block);
-    });
-});
-
-document.getElementById('nav').addEventListener('click', function showNav() {
-    const mobileNavbar = document.getElementById('mobile-navbar');
-    if (open) {
->>>>>>> a2a31d6a85e027d2e40c49d7969e584abc501dc1
         mobileNavbar.classList.remove('hidden');
         mobileNavbar.classList.add('visible');
     } else {
         mobileNavbar.classList.remove('visible');
         mobileNavbar.classList.add('hidden');
     }
-<<<<<<< HEAD
 };
-=======
-});
->>>>>>> a2a31d6a85e027d2e40c49d7969e584abc501dc1
